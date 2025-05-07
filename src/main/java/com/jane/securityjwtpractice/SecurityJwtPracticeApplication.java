b07c8b3571114c7280dfd3a01a0b1764edb3806b
Jane Lee <janelee1721@gmail.com>
2025-05-07 14:02:11 +0900

    🎉Init: Project for Spring Security + JWT

---
+++ src/main/java/com/jane/securityjwtpractice/SecurityJwtPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.jane.securityjwtpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityJwtPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityJwtPracticeApplication.class, args);
+    }
+
+}
